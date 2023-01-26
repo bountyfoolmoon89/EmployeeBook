b6de2d26af4201c112f62a65c8298a1d01520f24
Егор <bountyfoolmoon89@yandex.com>
2023-01-26 16:21:16 +0300

    Employee

---
+++ src/Employee.java
@@ -0,0 +1,47 @@
+import java.util.Arrays;
+
+public class Employee {
+    private final String FIO;
+    private int department;
+    private float salary;
+    private static int id = 100000;
+
+    public String getFIO() {
+        return FIO;
+    }
+
+    public int getDepartment() {
+        return department;
+    }
+
+    public float getSalary() {
+        return salary;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setDepartment(int department) {
+        this.department = department;
+    }
+
+    public void setSalary(float salary) {
+        this.salary = salary;
+    }
+
+    public static Employee[] filterByDepartment(int department){
+        return Arrays.stream(Book.employees).filter(x -> (x != null) && (x.getDepartment() == department)).toArray(Employee[]::new);
+    }
+
+    public Employee(String fio, int department, float salary){
+        this.FIO = fio;
+        this.department = department;
+        this.salary = salary;
+    }
+    @Override
+    public String toString() {
+        id++;
+        return "ID: " + id + " ФИО: " + FIO + " Отдел: " + department + " Зарплата: " + salary;
+    }
+}
